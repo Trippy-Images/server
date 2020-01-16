7e1d28755f7a47fef496c70ea4ca9d2b0d44ebb7
Immanuel Pandiangan <nuelpan@gmail.com>
2020-01-16 17:14:10 +0700

    add login endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const {generateToken} = require('../helpers/jwt')
+const {compare} = require('../helpers/bcrypt')
 
 class UserController {
   static async register(req, res, next) {
@@ -10,6 +11,20 @@ class UserController {
       })
       let token = generateToken({id: user._id})
       res.status(201).json({token})
+    } catch (error) {
+      next(error)
+    }
+  }
+  static async login(req, res, next) {
+    try {
+      const {email, password} = req.body
+      let user = await User.findOne({email})
+      if (!user || !compare(password, user.password)) {
+        next({ status: 404, message: 'Invalid Email or Password' })
+      } else {
+        let token = generateToken({ id: user._id })
+        res.status(200).json({token})
+      }
     } catch (error) {
       next(error)
     }
